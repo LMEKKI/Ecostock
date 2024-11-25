c770253db6de95d847feafbd274d593dd3006269
Hlmekki <hamdani.lmekki@gmail.com>
2024-11-25 11:27:11 +0100

    init easy admin

---
+++ src/Controller/OrderFormCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\OrderForm;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class OrderFormCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return OrderForm::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
