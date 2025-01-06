c2c1a4b3a7acbc5323f82563ec40975d3197cd4d
hlmekki <hamdani.lmekki@gmail.com>
2025-01-07 00:34:03 +0100

    ajout du service cart et du controller shoopingcart

--- src/Controller/CampingCrudController.php
+++ src/Controller/CampingCrudController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Camping;
+use App\Entity\SectionRestaurant;
+
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
---
+++ src/Service/Cart.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class Cart
+{
+  private const SESSION_KEY = 'cart';
+  private $session;
+
+  public function __construct(RequestStack $requestStack)
+  {
+    $this->session = $requestStack->getSession();
+  }
+
+  public function addToCart(array $item): void
+  {
+    $cart = $this->getCart();
+
+    foreach ($cart as &$cartItem) {
+      if ($cartItem['id'] === $item['id']) {
+        $cartItem['quantity'] += $item['quantity'];
+        $this->session->set(self::SESSION_KEY, $cart);
+        return;
+      }
+    }
+
+    $cart[] = $item;
+    $this->session->set(self::SESSION_KEY, $cart);
+  }
+
+  public function getCart(): array
+  {
+    return $this->session->get(self::SESSION_KEY, []);
+  }
+}
