f98fcb484878a368dd40b8bccf291c7d08767141
onsibuno <mathisb.buisson@gmail.com>
2025-01-08 08:53:30 +0100

    standby calcul

--- src/Controller/CalculController.php
+++ src/Controller/CalculController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\DataSheetRepository;
+use App\Repository\OrderFormRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,15 @@ use Symfony\Component\Routing\Attribute\Route;
 class CalculController extends AbstractController
 {
     #[Route('/calcul', name: 'app_calcul')]
-    public function index(): Response
+    public function index(OrderFormRepository $orderFormRepository, DataSheetRepository $dataSheetRepository): Response
     {
+
+        $recipes = $dataSheetRepository -> findAll();
+        $orders = $orderFormRepository -> findAll();
+
         return $this->render('calcul/index.html.twig', [
-            'controller_name' => 'CalculController',
+            'recipe' => $recipes,
+            'orders' => $orders,
         ]);
     }
 }
