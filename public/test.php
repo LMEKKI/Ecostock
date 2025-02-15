2a352857832e00507543b6846c149c20488dc8cf
hlmekki <hamdani.lmekki@gmail.com>
2025-02-15 15:36:29 +0100

    page pour commander fonctionel (front non fini attente design final)

--- public/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-  $pdo = new PDO('mysql:host=127.0.0.1:3306;dbname=eco', 'root', '');
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connexion réussie !";
-} catch (PDOException $e) {
-  echo "Erreur : " . $e->getMessage();
-}
